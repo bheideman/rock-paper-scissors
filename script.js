d6e35bfc8084369d5540b41f6d54f1719232b13d
bheideman <97909832+bheideman@users.noreply.github.com>
2025-06-03 14:27:32 -0400

    Create userChoice function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let user, computer
+
 function getComputerChoice(){
     let chance = Math.floor((Math.random() * 100) + 1)
-    let choice
+    let computerChoice
     if (chance <= 33 ) {
-        choice = "Rock"
+        computerChoice = "Rock"
     } else if (chance > 33 && chance <= 66) {
-        choice = "Paper"
+        computerChoice = "Paper"
     } else {
-        choice = "Scissors"
+        computerChoice = "Scissors"
     }
-    return console.log(choice)
+    return computer = computerChoice.toLowerCase
+}
+
+function getUserChoice() {
+    let userChoice = prompt("Type rock, paper, or scissors: ")
+    return user = userChoice.toLowerCase()
 }
