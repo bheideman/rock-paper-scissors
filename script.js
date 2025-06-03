02d17e52b7e66d1913d563ce276adab6ca8d3bf8
bheideman <97909832+bheideman@users.noreply.github.com>
2025-06-03 14:57:45 -0400

    Create playGame function to play 5 rounds, track score, declare winner

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let user, computer
+
+
+function capitalize(text) {
+            first = text[0].toUpperCase()
+            rest = text.slice(1, text.length).toLowerCase()
+            return first + rest
+        }
 
 function getComputerChoice(){
     let chance = Math.floor((Math.random() * 100) + 1)
@@ -10,10 +16,46 @@ function getComputerChoice(){
     } else {
         computerChoice = "Scissors"
     }
-    return computer = computerChoice.toLowerCase
+    return computerChoice
 }
 
 function getUserChoice() {
     let userChoice = prompt("Type rock, paper, or scissors: ")
-    return user = userChoice.toLowerCase()
+    return userChoice
 }
+
+function playGame(){
+    let rounds = 0
+    let userScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+        computerChoice = getComputerChoice().toLowerCase()
+        humanChoice = getUserChoice().toLowerCase()
+    
+        if (((humanChoice === "rock" && computerChoice === "paper") || humanChoice === "paper" && computerChoice === "scissors") || humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose! " + capitalize(computerChoice) + " beats " + capitalize(humanChoice) + ".")
+            computerScore += 1
+        } else if (((computerChoice === "rock" && humanChoice === "paper") || computerChoice === "paper" && humanChoice === "scissors") || computerChoice === "scissors" && humanChoice === "rock") {
+            console.log("You win! " + capitalize(humanChoice) + " beats " + capitalize(computerChoice) + ".")
+            userScore += 1
+        } else {
+            console.log("Tie! " + capitalize(humanChoice) + " ties with " + capitalize(computerChoice) + ". Try again!")
+        }
+    } 
+
+    while (rounds < 5) {
+        playRound()
+        rounds += 1
+    }
+
+    if (userScore > computerScore) {
+        console.log("The score is " + userScore + " to " + computerScore + ". Congratulations! You win!")
+    } else if (userScore < computerScore) {
+        console.log("The score is " + userScore + " to " + computerScore + ". Sorry, you lose!")
+    } else {
+        console.log("The score is " + userScore + " to " + computerScore + ". Looks like a tie!")
+    }
+}
+
+playGame()
